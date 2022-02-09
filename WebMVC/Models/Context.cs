c52800e3150dafacdf59208b61a0c6caf9b2ca23
Nicolas Biletzki <nicolasbiletzki@gmail.com>
2022-02-09 15:07:50 -0300

    Adicionando DbContext

---
+++ WebMVC/Models/Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+
+
+        //Configurar o Entity Framework
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrated Security=True;");
+        }
+    }
+}
