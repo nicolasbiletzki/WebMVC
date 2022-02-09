e22207c0ab5c555103e36de4403892510dbe0168
Nicolas Biletzki <nicolasbiletzki@gmail.com>
2022-02-09 15:14:00 -0300

    Criando DbContext e Iniciando Migrations

--- WebMVC/Startup.cs
+++ WebMVC/Startup.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebMVC.Models;
 
 namespace WebMVC
 {
@@ -24,6 +25,7 @@ namespace WebMVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<Context>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
